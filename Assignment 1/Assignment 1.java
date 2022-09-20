31cc89cf3ba54e7350b98c874e654f81c023a509
Ben Smith <76930048+BenSmith278@users.noreply.github.com>
2022-09-20 17:46:44 -0400

    Add fibIter

--- Assignment 1/Assignment 1.java
+++ Assignment 1/Assignment 1.java
@@ -1,4 +1,23 @@
 class FibSeq {
     public static void main(String[] Args) {
+        System.out.println(fibIter(4));
+    }
+
+    public static int fibIter(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        int prev = 1;
+        int cur = 1;
+        int temp;
+
+        for (int i=0; i<n; i++) {
+            temp = cur;
+            cur = prev + cur;
+            prev = temp;
+        }
+        
+        return cur;
     }
 }
