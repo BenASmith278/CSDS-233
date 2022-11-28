f4fdb4b8dd02db2a3baa9bfa4738c0140609c4b7
Ben Smith <76930048+BenSmith278@users.noreply.github.com>
2022-11-28 17:36:02 -0500

    Create method skeletons

---
+++ Assignment 5/Sort.java
@@ -0,0 +1,85 @@
+/**
+ * <h1> Sorting Algorithms </h1>
+ * 
+ * The methods sort an int array using
+ * their respective algorithm. The 
+ * efficiencies of the algorithms will
+ * be benchmarked and analyzed.
+ * 
+ * @author Ben Smith
+ * @version 0.0
+ * @since 11-27-2022
+ */
+
+public class Sort {
+
+    /*
+     * Uses insertion sort algorithm to sort the
+     * input array arr in descending order. Time 
+     * complexity should be O(n^2).
+     */
+    void insertionSort(int[] arr) {
+    }
+
+    /*
+     * Uses bubble sort algorithm to sort the input
+     * array arr in descending order. Time complexity
+     * should be O(n^2) or O(n) best case.
+     */
+    void bubbleSort(int[] arr) {
+
+
+    }
+
+    /*
+     * Uses shell sort algorithm using Hibbard's sequence 
+     * to sort the input array arr in descending order. 
+     * Time complexity should be O(nlog^2(n)).
+     */
+    void shellSort(int[] arr) {
+    }
+
+    /*
+     * Uses quick sort algorithm to sort the input 
+     * array arr into descending order. Time 
+     * complexity should be O(nlogn) average or O(n^2)
+     * worst case.
+     */
+    void quickSort(int[] arr) {
+
+    }
+
+    /*
+     * Uses merge sort algorithm to sort the input array
+     * arr into descending order. Time complexity should
+     * be O(nlogn).
+     */
+    void mergeSort(int[] arr) {
+
+    }
+
+    /*
+     * Uses the upgraded quick sort agorithm to sort the
+     * input array arr into descending order. Time complexity
+     * should be O(nlogn)
+     */
+    void upgradedQuickSort(int[] arr) {
+
+    }
+
+    /*
+     * Takes random input and generates a randomized 
+     * array of size n.
+     */
+    int[] generateRandomArray(int n) {
+        int[] arr = new int[n];
+        return arr;
+    }
+
+    /*
+     * 
+     */
+    void readCommands(String filepath) {
+
+    }
+}
